6ccbf6d47c2e48108d58b104a6ef6e4bb9da0037
ScottXileChen <scottxilechen@gmail.com>
2022-03-14 15:59:41 -0700

    Scott_Chen
    - Updated GridController to have better debug draw.

--- War of Xiwan/Assets/Scripts/FlowField/GridController.cs
+++ War of Xiwan/Assets/Scripts/FlowField/GridController.cs
@@ -6,12 +6,27 @@ namespace WOX.FlowField
 {
     public class GridController : MonoBehaviour
     {
+        public enum DebugDrawWay
+        {
+            Cost,
+            BestCost,
+            BestDirection
+        }
+
+        [Header("Attribute Info")]
         [SerializeField]
         private Camera lookCamera = null;
         public Vector2 gridStartPosition = Vector2.zero;
         [Min(0)]
         public Vector2Int gridSize = Vector2Int.one;
         public float cellRadius = 0.5f;
+
+        [Header("Debug Info")]
+        [SerializeField]
+        private bool debugDrawEnable = false;
+        [SerializeField]
+        private DebugDrawWay debugDrawWay = DebugDrawWay.BestDirection;
+
         public FlowField CurrentFlowField { get; private set; }
 
         private bool initialized = false;
@@ -42,7 +57,7 @@ namespace WOX.FlowField
 
         private void OnDrawGizmos()
         {
-            if (initialized)
+            if (debugDrawEnable && initialized)
             {
                 WOX.Utils.DebugRendererHelper.DrawGrid(CurrentFlowField, Color.red);
             }
@@ -50,34 +65,42 @@ namespace WOX.FlowField
 
         private void OnGUI()
         {
-            GUIStyle style = new GUIStyle(GUI.skin.label);
-            style.alignment = TextAnchor.MiddleCenter;
-
-            if (initialized)
+            if (debugDrawEnable && initialized)
             {
+                GUIStyle style = new GUIStyle(GUI.skin.label);
+                style.alignment = TextAnchor.MiddleCenter;
+
                 foreach (Cell currentCell in CurrentFlowField.Grid)
                 {
-                    if (currentCell.BestDirection == GridDirection.None)
-                        UnityEditor.Handles.Label(currentCell.WorldPosition, "X", style);
-                    else if (currentCell.BestDirection == GridDirection.North)
-                        UnityEditor.Handles.Label(currentCell.WorldPosition, "¡ü", style);
-                    else if (currentCell.BestDirection == GridDirection.South)
-                        UnityEditor.Handles.Label(currentCell.WorldPosition, "¡ý", style);
-                    else if (currentCell.BestDirection == GridDirection.East)
-                        UnityEditor.Handles.Label(currentCell.WorldPosition, "¡ú", style);
-                    else if (currentCell.BestDirection == GridDirection.West)
-                        UnityEditor.Handles.Label(currentCell.WorldPosition, "¡û", style);
-                    else if (currentCell.BestDirection == GridDirection.NorthEast)
-                        UnityEditor.Handles.Label(currentCell.WorldPosition, "¨J", style);
-                    else if (currentCell.BestDirection == GridDirection.NorthWest)
-                        UnityEditor.Handles.Label(currentCell.WorldPosition, "¨I", style);
-                    else if (currentCell.BestDirection == GridDirection.SouthEast)
-                        UnityEditor.Handles.Label(currentCell.WorldPosition, "¨K", style);
-                    else if (currentCell.BestDirection == GridDirection.SouthWest)
-                        UnityEditor.Handles.Label(currentCell.WorldPosition, "¨L", style);
-
-                    //UnityEditor.Handles.Label(currentCell.WorldPosition, currentCell.BestCost.ToString(), style);
-
+                    if (debugDrawWay == DebugDrawWay.BestDirection)
+                    {
+                        if (currentCell.BestDirection == GridDirection.None)
+                            UnityEditor.Handles.Label(currentCell.WorldPosition, "X", style);
+                        else if (currentCell.BestDirection == GridDirection.North)
+                            UnityEditor.Handles.Label(currentCell.WorldPosition, "¡ü", style);
+                        else if (currentCell.BestDirection == GridDirection.South)
+                            UnityEditor.Handles.Label(currentCell.WorldPosition, "¡ý", style);
+                        else if (currentCell.BestDirection == GridDirection.East)
+                            UnityEditor.Handles.Label(currentCell.WorldPosition, "¡ú", style);
+                        else if (currentCell.BestDirection == GridDirection.West)
+                            UnityEditor.Handles.Label(currentCell.WorldPosition, "¡û", style);
+                        else if (currentCell.BestDirection == GridDirection.NorthEast)
+                            UnityEditor.Handles.Label(currentCell.WorldPosition, "¨J", style);
+                        else if (currentCell.BestDirection == GridDirection.NorthWest)
+                            UnityEditor.Handles.Label(currentCell.WorldPosition, "¨I", style);
+                        else if (currentCell.BestDirection == GridDirection.SouthEast)
+                            UnityEditor.Handles.Label(currentCell.WorldPosition, "¨K", style);
+                        else if (currentCell.BestDirection == GridDirection.SouthWest)
+                            UnityEditor.Handles.Label(currentCell.WorldPosition, "¨L", style);
+                    }
+                    else if (debugDrawWay == DebugDrawWay.BestCost)
+                    {
+                        UnityEditor.Handles.Label(currentCell.WorldPosition, currentCell.BestCost.ToString(), style);
+                    }
+                    else if (debugDrawWay == DebugDrawWay.Cost)
+                    {
+                        UnityEditor.Handles.Label(currentCell.WorldPosition, currentCell.Cost.ToString(), style);
+                    }
                 }
             }
         }
